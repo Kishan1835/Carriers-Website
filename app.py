f5a06402ceb0d0d43d4b136c7b30f091224a7d78
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2023-09-25 09:31:58 +0530

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask # import flask
+from flask import Flask, render_template # import flask
+
 
 app = Flask(__name__) # create an app instance
 
 @app.route("/") # at the end point /
 def hello_World(): # call method hello_world
-    return "hello Kishan" # which returns "hello world" 
+    return render_template('home.html') 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
